e21959d6d0898354022d0a0708e508747aa2a6ec
lzq86630 <lzq86630@126.com>
2016-05-24 18:01:29 +0800

    新增aop模块
    aop记录web层访问请求和返回数据

---
+++ src/main/java/com/springboot/learn/aop/WebLogAspect.java
@@ -0,0 +1,60 @@
+/**
+ * CitizenCard.com Inc.
+ * Copyright (c) 2004-2016 All Rights Reserved.
+ */
+package com.springboot.learn.aop;
+
+import java.util.Arrays;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+/**
+ * web层AOP,记录请求和返回结果日志
+ * @author liuzq
+ * @version $Id: WebLogAspect.java, v 0.1 2016年5月24日 下午5:41:12 liuzq Exp $
+ */
+public class WebLogAspect {
+
+    /** 日志 */
+    private static final Logger logger    = LoggerFactory.getLogger(WebLogAspect.class);
+
+    /** 同步计算每次请求时间 */
+    ThreadLocal<Long>           startTime = new ThreadLocal<>();
+
+    @Pointcut("execution(public * com.springboot.learn.web..*.*(..))")
+    public void webLog() {
+    }
+
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        startTime.set(System.currentTimeMillis());
+        // 接收到请求，记录请求内容
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder
+            .getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        // 记录下请求内容
+        logger.info("URL : " + request.getRequestURL().toString());
+        logger.info("HTTP_METHOD : " + request.getMethod());
+        logger.info("IP : " + request.getRemoteAddr());
+        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "."
+                    + joinPoint.getSignature().getName());
+        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+    }
+
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
+        // 处理完请求，返回内容
+        logger.info("RESPONSE : " + ret);
+        logger.info("SPEND TIME : " + (System.currentTimeMillis() - startTime.get()));
+    }
+}
