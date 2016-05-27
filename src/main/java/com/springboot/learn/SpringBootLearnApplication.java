411c0c5ca4f710b9cb3121b949eaf3f89c6399f7
lzq86630 <lzq86630@126.com>
2016-05-27 17:09:37 +0800

    war+bat

--- src/main/java/com/springboot/learn/SpringBootLearnApplication.java
+++ src/main/java/com/springboot/learn/SpringBootLearnApplication.java
@@ -2,15 +2,26 @@ package com.springboot.learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication
-public class SpringBootLearnApplication {
+public class SpringBootLearnApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLearnApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLearnApplication.class, args);
+    }
+    
+    /** 
+     * spring boot打成war包时需要方法,相当于web.xlm中配置加载spring上下文初始化servlet
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootLearnApplication.class);
+    }
+
 }
