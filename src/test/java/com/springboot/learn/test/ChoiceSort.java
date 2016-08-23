f069c1db2bbaacabcda07a92264de01b02addf0d
lzq86630 <lzq86630@126.com>
2016-08-23 14:02:28 +0800

    jsonUtil

--- src/test/java/com/springboot/learn/test/ChoiceSort.java
+++ src/test/java/com/springboot/learn/test/ChoiceSort.java
@@ -10,7 +10,7 @@ package com.springboot.learn.test;
  * @version $Id: ChoiceSort.java, v 0.1 2016年8月15日 下午4:54:15 liuzq Exp $
  */
 public class ChoiceSort {
-    public static void _choiceSort(int[] a) {
+    public static void choiceSort(int[] a) {
         if (a == null || a.length <= 0) {
             return;
         }
@@ -33,7 +33,7 @@ public class ChoiceSort {
     public static void main(String[] args) {
         int a[] = { 49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 5, 4, 62, 99, 98, 54, 56, 17,
                     18, 23, 34, 15, 35, 25, 53, 51 };
-        ChoiceSort._choiceSort(a);
+        ChoiceSort.choiceSort(a);
         for (int i = 0; i < a.length; i++)
             System.out.println(a[i]);
     }
